e6b38fc3c8a516c1ffc853e81d4910ec825137f6
gliefsoe <gert.liefsoens@telenet.be>
2022-12-27 18:34:12 +0100

    first draft install
    first draft install 1.0

---
+++ server/index.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const http = require('http');
+const WebSocket = require('ws');
+const path = require('path');
+const os = require('os');
+const app = express();
+//const server = http.createServer(app);
+//const wss = new WebSocket.Server({ server });
+const wss = new WebSocket.Server({ port: 8080 });
+
+// store the connection Count
+let connectionCount = 0;
+// store the WebSocket objects for each client
+const clients = {};
+
+wss.on('connection', (ws, req) => {
+    // increment the connection count
+    connectionCount++;
+    // assign a unique identifier to the client
+    const clientId = req.headers['sec-websocket-key'];
+
+    // store the client's WebSocket object
+    clients[clientId] = ws;
+
+    console.log(`New WebSocket connection: ${clientId}`);
+
+    // log the connection count
+    console.log(`Number of connections: ${connectionCount}`)
+    console.log(`Client connected remoteaddress: ${req.connection.remoteAddress}`);
+
+  ws.on('message', (message) => {
+    
+    console.log(`Received message from ${clientId}: ${message}`);
+    console.log(`Received message: ${message}`);
+    ws.send(`Echo: ${message}`);
+  });
+
+  ws.on('close', () => {
+    console.log('Client disconnected');
+    connectionCount--;
+
+    console.log(`WebSocket connection closed: ${clientId}`);
+    // remove the client from the clients object
+    delete clients[clientId];
+
+    console.log(`Number of connections: ${connectionCount}`);
+  });
+});
+
+// Home page route
+app.get('/', (req, res) => {
+  //res.send('Hello World');
+  //res.send(`Hello World<br>Server IP: ${os.networkInterfaces().eth0[0].address}`);
+  res.sendFile(path.join(__dirname, 'start.html'));
+  
+});
+
+// Game page route
+app.get('/game', (req, res) => {
+  console.log('starting game and Searching for second player...');
+  
+  
+  res.sendFile('game\\client.html', { root: __dirname });
+
+});
+
+console.log(`Root of server: ${__dirname}`);
+
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+  console.log('Server IP:');
+  console.log('networkInterfaces' + JSON.stringify(os.networkInterfaces()));
+
+});
